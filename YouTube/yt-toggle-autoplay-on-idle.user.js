e07bffd2d2d69bc4c1cfd466b536c86dc29053be
cybergene <cybergene@protonmail.com>
2025-07-26 01:13:45 +0900

    Fixes autoplay toggle selection
    Addresses an issue where the script fails to correctly select the autoplay toggle button due to a change in YouTube's UI.
    
    Updates the selector to correctly identify the toggle and ensure that the autoplay functionality can be disabled when the user is idle.

--- YouTube/yt-toggle-autoplay-on-idle.user.js
+++ YouTube/yt-toggle-autoplay-on-idle.user.js
@@ -5,6 +5,8 @@
 // @description  一定時間無操作ならYouTubeの次動画再生を止める
 // @match        https://www.youtube.com/*
 // @grant        none
+// @author       cybergene
+// @source       https://github.com/cyber-gene/TampermonkeyUserScripts
 // @downloadURL  https://raw.githubusercontent.com/cyber-gene/TampermonkeyUserScripts/main/YouTube/yt-toggle-autoplay-on-idle.user.js
 // ==/UserScript==
 
@@ -27,8 +29,10 @@
         const idleThreshold = idleMinutes * 60 * 1000;
 
         if (idleMs > idleThreshold) {
-            const autoplayToggle = document.querySelector('button.ytp-autonav-toggle-button');
-            if (autoplayToggle && autoplayToggle.getAttribute('aria-checked') === 'true') {
+            const autoplayToggle = document.querySelector('button.ytp-autonav-toggle');
+            const toggleButton = autoplayToggle?.querySelector('.ytp-autonav-toggle-button');
+            
+            if (autoplayToggle && toggleButton && toggleButton.getAttribute('aria-checked') === 'true') {
                 autoplayToggle.click(); // 自動再生オフ
                 console.log('[YouTube AutoStop] 自動再生をオフにしました（無操作検出）');
             }

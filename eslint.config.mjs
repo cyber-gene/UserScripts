8a215c6079df4e1183d7869df9dee7109fe3b815
cybergene <cybergene@protonmail.com>
2025-07-26 16:18:06 +0900

    Configures ESLint with Prettier for code formatting
    Updates ESLint configuration to integrate Prettier for consistent code formatting.
    This includes installing and configuring the necessary ESLint plugins and configurations
    to enforce Prettier's formatting rules during linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,21 @@
 import js from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    languageOptions: { globals: globals.browser },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "prettier/prettier": "error",
+      // 追加のルールはここに記述
+    }
+  },
 ]);
